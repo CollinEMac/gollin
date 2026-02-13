5ffc78e48544d5ab441a0b0ab01419c45a6c9a42
CollinEMac <macd.collin@gmail.com>
2026-02-13 00:49:39 -0600

    intial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CollinEMac/gollin
+
+go 1.25.7
---
+++ gollin.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "log"
+    "strings"
+)
+
+func main() {
+    // reads the contents of gollin at the given path and generates a .go file
+    fmt.Println("Begin parsing");
+
+    if len(os.Args) < 2 {
+        log.Fatal("gollin file path required.")
+        os.Exit(1)
+    }
+    filePath := os.Args[1]
+
+    var gollinPath string
+
+    if strings.HasSuffix(filePath, ".gollin") {
+       gollinPath = filePath
+    } else {
+        // Build the full filepath
+        // Should probably check for extensions before doing this
+        var gollinBuilder strings.Builder
+        gollinBuilder.WriteString(filePath)
+        gollinBuilder.WriteString(".gollin")
+        gollinPath = gollinBuilder.String()
+    }
+
+    // Get gollin code
+    code, err := os.ReadFile(gollinPath)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Manipulate the code here
+    // parse()
+
+    // we know gollinPath has suffix .gollin at this point
+    newFilePath := strings.Split(gollinPath, ".")[0]
+    var goPath strings.Builder
+    goPath.WriteString(newFilePath)
+    goPath.WriteString(".go")
+
+    // Spit out the go code
+    os.WriteFile(goPath.String(), code, 0777);
+}
+
+func parse(filePath string) {
+    return
+}
